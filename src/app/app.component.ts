b70e178cac62a6d013e03c749e41c067aa88fd33
Mursaleen00 <Hafsanoorofficial06@gmail.com>
2024-09-16 12:58:10 +0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';  // Import RouterModule
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  standalone: true,
+  imports: [RouterModule]  // Import RouterModule here
 })
 export class AppComponent {
-  title = 'exclusive-site';
+  title = 'your-app-name';
 }
